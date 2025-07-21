c20ce28edbae6e1c63285d50c97a5f0717e1b6c0
SlimeNull <slimenull@outlook.com>
2025-07-21 09:56:39 +0800

    fix: 修复 HLSL 函数定义顺序

--- EleCho.MegaTextures/Shaders/AetherTexImage.hlsl
+++ EleCho.MegaTextures/Shaders/AetherTexImage.hlsl
@@ -37,48 +37,6 @@ struct vs_out
     float2 texcoord : MT_TEXCOORD;
 };
 
-float4 sample_source(int sourceIndex, float2 texcoord)
-{
-    int tileX = ((int) texcoord.x) / TileWidth;
-    int tileY = ((int) texcoord.y) / TileHeight;
-    int tileIndex = tileY * TileColumns + tileX;
-    
-    float tileXStart = TileWidth * tileX;
-    float tileYStart = TileHeight * tileY;
-    float xInTile = texcoord.x - tileXStart;
-    float yInTile = texcoord.y - tileYStart;
-    float uInTile = xInTile / TileWidth;
-    float vInTile = yInTile / TileHeight;
-    
-    return _input[sourceIndex].Sample(_sampler, float3(uInTile, vInTile, tileIndex));
-}
-
-vs_out vs_main(vs_in input)
-{
-    vs_out output =
-    {
-        float4(input.position, 0.5, 1.0),
-        input.texcoord,
-    };
-
-    return output;
-}
-
-float4 ps_main(vs_out input) : SV_TARGET
-{
-    float4 sources[SourceCount];
-    
-    [unroll]
-    for (int i = 0; i < SourceCount; i++)
-    {
-        sources[i] = sample_source(i, input.texcoord);
-    }
-
-    float4 result = SourceExpr;
-    
-    return result;
-}
-
 // color
 float4 color(float value)
 {
@@ -492,4 +450,46 @@ float4 mul_rgb_to_a(float3 v)
 float4 mul_rgb_to_a(float4 v)
 {
     return mul_rgb_to_a(v.rgb);
+}
+
+float4 sample_source(int sourceIndex, float2 texcoord)
+{
+    int tileX = ((int) texcoord.x) / TileWidth;
+    int tileY = ((int) texcoord.y) / TileHeight;
+    int tileIndex = tileY * TileColumns + tileX;
+    
+    float tileXStart = TileWidth * tileX;
+    float tileYStart = TileHeight * tileY;
+    float xInTile = texcoord.x - tileXStart;
+    float yInTile = texcoord.y - tileYStart;
+    float uInTile = xInTile / TileWidth;
+    float vInTile = yInTile / TileHeight;
+    
+    return _input[sourceIndex].Sample(_sampler, float3(uInTile, vInTile, tileIndex));
+}
+
+vs_out vs_main(vs_in input)
+{
+    vs_out output =
+    {
+        float4(input.position, 0.5, 1.0),
+        input.texcoord,
+    };
+
+    return output;
+}
+
+float4 ps_main(vs_out input) : SV_TARGET
+{
+    float4 sources[SourceCount];
+    
+    [unroll]
+    for (int i = 0; i < SourceCount; i++)
+    {
+        sources[i] = sample_source(i, input.texcoord);
+    }
+
+    float4 result = SourceExpr;
+    
+    return result;
 }
