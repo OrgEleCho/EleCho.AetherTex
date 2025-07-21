8c632c5393424b628e5667774cdf668945d16b1e
SlimeNull <slimenull@outlook.com>
2025-07-21 14:07:17 +0800

    feat: 添加速度测试代码

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.Diagnostics;
 using System.Numerics;
 using EleCho.AetherTex;
 using SkiaSharp;
@@ -12,7 +13,7 @@ internal class Program
         return new TextureData(TextureFormat.Bgra8888, bitmap.Width, bitmap.Height, bitmap.GetPixels(), bitmap.RowBytes);
     }
 
-    private static void Main(string[] args)
+    static void TestFeature()
     {
         var bitmap = SKBitmap.Decode("test.jpg");
         var bitmap2 = new SKBitmap(512, 512, SKColorType.Bgra8888, SKAlphaType.Unpremul);
@@ -32,6 +33,35 @@ internal class Program
         var source = megaTexture.CreateSource("color.bgr");
         megaTexture.Read(source, quad, GetTextureData(bitmap2));
         bitmap2.Encode(output, SKEncodedImageFormat.Png, 100);
+
+    }
+
+    static void TestSpeed()
+    {
+        var megaTexture = new AetherTexImage(TextureFormat.Bgra8888, 8192, 8192, 3, 3);
+        var bufferBitmap = new SKBitmap(300, 300, SKColorType.Bgra8888, SKAlphaType.Unpremul);
+        var bufferData = GetTextureData(bufferBitmap);
+
+        var quad = megaTexture.FullQuad;
+        var stopwatch = Stopwatch.StartNew();
+        var readCount = 0;
+        while (true)
+        {
+            megaTexture.Read(quad, bufferData);
+            readCount++;
+
+            if (stopwatch.ElapsedMilliseconds >= 1000)
+            {
+                Console.WriteLine($"FPS: {readCount}");
+                stopwatch.Restart();
+                readCount = 0;
+            }
+        }
+    }
+
+    private static void Main(string[] args)
+    {
+        TestSpeed();
 
         Console.WriteLine("Hello, World!");
     }
